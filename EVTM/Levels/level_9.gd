6e3759c3042410961b197c044f5161de704e3b1b
Rafael Félix <rafaelfelix1162@gmail.com>
2026-01-17 17:42:07 +0100

    Fix

--- EVTM/Levels/level_9.gd
+++ EVTM/Levels/level_9.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var start = $Start
 @onready var end = $End
 
-const ID := 6
+const ID := 9
 
 func _ready():
 	var player := get_tree().get_first_node_in_group("player")
@@ -12,6 +12,5 @@ func _ready():
 	player.global_position = getStartPosition(GameState.came_from(ID))
 
 func getStartPosition(c: int):
-	if c == 8: return end.global_position
-	if c == 10: return start.global_position
+	if c == 10: return end.global_position
 	return start.global_position
