648eddf31a9ec2215ef23289fc8a3ebbc74efd76
Anthony Wernli <71585688+anthony-we-ch@users.noreply.github.com>
2026-01-17 17:29:14 +0100

    Added Level 9

---
+++ EVTM/Levels/level_9.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var start = $Start
+@onready var end = $End
+
+const ID := 6
+
+func _ready():
+	var player := get_tree().get_first_node_in_group("player")
+	if !player:
+		return false
+	player.global_position = getStartPosition(GameState.came_from(ID))
+
+func getStartPosition(c: int):
+	if c == 8: return end.global_position
+	if c == 10: return start.global_position
+	return start.global_position
